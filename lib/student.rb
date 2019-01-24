a8900462794cf4921c0a89ba1ad1038ad6bc503a
Milena Kari <milenakari@yahoo.com>
2019-01-24 04:35:49 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -74,6 +74,13 @@ class Student
     end
   end
 
+  def self.all_students_in_grade_X(x)
+    sql = "SELECT * FROM students WHERE grade = ?"
+    DB[:conn].execute(sql, x).map do |row|
+      self.new_from_db(row)
+    end
+  end
+
   def self.first_student_in_grade_10
     self.first_X_students_in_grade_10(1).first
   end
